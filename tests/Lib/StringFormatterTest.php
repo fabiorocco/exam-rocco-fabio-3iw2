07fe794cb41eab958bd850448105b1587fbac618
“fabiorocco” <fabio.rocco@green-conseil.com>
2017-02-01 11:51:43 +0100

    Write all method for starting tests

--- tests/Lib/StringFormatterTest.php
+++ tests/Lib/StringFormatterTest.php
@@ -5,8 +5,43 @@ use Lib\StringFormatter;
 
 class StringFormatterTest extends \PHPUnit_Framework_TestCase
 {
-    public function testPrefix()
+    public function testPrefixSuccess()
     {
-        var_dump('coucou');
+
+    }
+
+    public function testPrefixError()
+    {
+
+    }
+
+    public function testSuffixSuccess()
+    {
+
+    }
+
+    public function testSuffixError()
+    {
+
+    }
+
+    public function testToCamelCaseSuccess()
+    {
+
+    }
+
+    public function testToCamelCaseError()
+    {
+
+    }
+
+    public function testConcatStringSuccess()
+    {
+
+    }
+
+    public function testConcatStringError()
+    {
+
     }
 }
